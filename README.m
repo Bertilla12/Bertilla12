d5f06f5b8eed7216c410b69d005e837247c11bbd
Bertilla12 <144876632+Bertilla12@users.noreply.github.com>
2023-09-13 11:49:20 +0530

    My Short Intro

---
+++ README.m
@@ -0,0 +1,11 @@
+- 👋 Hi, I’m @Bertilla12
+- 👀 I’m interested in backend projects
+- 🌱 I’m currently learning stock market analysis
+- 📫 How to reach me ...
+- bertillasharon@gmail.com
+- I'm an electonic & communication student 
+
+<!---
+Bertilla12/Bertilla12 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
